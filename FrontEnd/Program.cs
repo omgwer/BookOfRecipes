fd5253742d680ce0f4f6fe361cbd92d98e73c0fe
omgwer <63747048+omgwer@users.noreply.github.com>
2022-05-10 13:32:06 +0300

    init

---
+++ FrontEnd/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var builder = WebApplication.CreateBuilder( args );
+var app = builder.Build();
+
+/*app.AddSpaStaticFiles( configuration =>
+{
+    configuration.RootPath = "ClientApp/dist";
+} );*/
+          
+app.UseDeveloperExceptionPage();
+          
+app.UseStaticFiles();          
+
+app.UseSpa( spa =>
+{
+    spa.Options.SourcePath = "ClientApp";               
+    spa.UseAngularCliServer( npmScript: "start" );                
+} );
+ 
+app.Run();
