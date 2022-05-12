e545ee611c6742861fa688827941bd95c5137290
omgwer <63747048+omgwer@users.noreply.github.com>
2022-05-12 11:39:54 +0300

    upd

--- FrontEnd/ClientApp/src/app/app.component.ts
+++ FrontEnd/ClientApp/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
--- FrontEnd/ClientApp/src/app/app.module.ts
+++ FrontEnd/ClientApp/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonComponent } from './shared/button/button.component';
-import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TopBlockComponent } from './components/top-block/top-block.component';
+import { TagsBlockComponent } from './components/tags-block/tags-block.component';
 
 @NgModule({
-  declarations: [
-    AppComponent, 
-    ButtonComponent, 
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, TopBlockComponent, TagsBlockComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
